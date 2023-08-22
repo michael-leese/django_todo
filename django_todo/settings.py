03df9ea7aaa62b3fb1e5125b397420c8c0c9feab
Michael Leese <michael-leese@outlook.com>
2023-08-22 17:00:44 +0100

    attempt fix allowed_hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ if "RUN_PRODUCTION" in os.environ:
     # SECURITY WARNING: keep the secret key used in production secret!
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
+    DEBUG = True
 else:
     print("Running locally use env.py for keys")
     SECRET_KEY = SECRET_KEY
