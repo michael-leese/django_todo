9405ea4407820f0935d287b390eaf27fe183f93d
Michael Leese <michael-leese@outlook.com>
2023-09-06 12:24:14 +0100

    change url for allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -43,7 +43,7 @@ else:
     print("Get local env.py setting")
     DB_URL = DB_URL
     
-ALLOWED_HOSTS = ['https://intense-taiga-13993-89b2a51cb297.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', 'intense-taiga-13993-89b2a51cb297.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = [DB_URL]
 # CSRF_TRUSTED_ORIGINS = ['https://{}'.format(DB_URL)]
 
