4d032e030296364efeacc75584e296aefe0a87dc
Michael Leese <michael-leese@outlook.com>
2023-09-06 12:21:32 +0100

    add heroku url

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -43,7 +43,7 @@ else:
     print("Get local env.py setting")
     DB_URL = DB_URL
     
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://intense-taiga-13993-89b2a51cb297.herokuapp.com/']
 CSRF_TRUSTED_ORIGINS = [DB_URL]
 # CSRF_TRUSTED_ORIGINS = ['https://{}'.format(DB_URL)]
 
